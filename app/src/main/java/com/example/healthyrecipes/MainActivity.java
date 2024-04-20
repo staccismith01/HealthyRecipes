d38fadd61622cb8763349ad4bb64b83acfef76af
staccismith01 <staccismith3201@gmail.com>
2024-04-19 21:23:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/healthyrecipes/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.healthyrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.bttnRecipe);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( MainActivity.this, Recipe.class));
+            }
+        });
+    }
+}
